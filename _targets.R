a6ede708384db9a56069e2acca6c98537dc8eeee
Mateus Harrington <Bernardo-HarringtonG@cardiff.ac.uk>
2025-05-15 12:31:48 +0100

    Add targets/renv toy example

---
+++ _targets.R
@@ -0,0 +1,10 @@
+library(targets)
+library(tarchetypes)
+tar_source()
+tar_option_set(packages = c("readr", "dplyr", "ggplot2"))
+list(
+  tar_target(file, "data/data.csv", format = "file"),
+  tar_target(data, get_data(file)),
+  tar_target(model, fit_model(data)),
+  tar_target(plot, plot_model(model, data))
+)
