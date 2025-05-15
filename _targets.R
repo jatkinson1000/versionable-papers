a21acebbb9bb7d4afb726c1e631f7792eaf8d078
Richard J. Acton <richardjacton@richardjacton.net>
2025-05-15 15:07:37 +0100

    env update + typo

--- _targets.R
+++ _targets.R
@@ -1,7 +1,7 @@
 library(targets)
 library(tarchetypes)
 tar_source()
-tar_option_set(packages = c("readr", "dplyr", "ggplot2","rCAT","dplyr","sf","leaflet","htmlwidgets"))
+tar_option_set(packages = c("readr", "dplyr", "ggplot2","rCAT2","dplyr","sf","leaflet","htmlwidgets"))
 list(
   tar_target(previous_data, "data/Pachypodium_rosulatum/previous_data.rds", format = "file"),
   tar_target(new_recs, "data/Pachypodium_rosulatum/new_records_20250515_102643.rds", format = "file"),
@@ -11,7 +11,7 @@ list(
   # tar_target(map_html, htmlwidgets::saveWidget(map, file = "map_test.html", selfcontained = TRUE)),
   tar_render(
     paper,
-    "example.qmd"#,
+    "example.qmd",
     # Can control output file location like so:
     output_file = "pages/example.html"
   )
